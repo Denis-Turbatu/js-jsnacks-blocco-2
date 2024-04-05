449c41e3c33f8fcfb4c0772b49e602b4747dcc53
Denis-Turbatu <turbatudenis81@gmail.com>
2024-04-05 14:54:10 +0200

    fix: no default function js used

--- jsnack3/functions.js
+++ jsnack3/functions.js
@@ -1,15 +1,16 @@
 
 // Aggiunge gli elementi
+let countMaxElem = 0;
 function addElementi(numIter) {
     let i = 0;
-
     do {
         const addElem = prompt("Inserisci");
         newArray.push(addElem);
         i++;
     } while (i < numIter);
+    countMaxElem = i;
 }
 
 function contaElementi() {
-    return newArray.length;
+    return countMaxElem;
 }
